5b9c0164af732842bd4bc8dce3989c00881cfbb4
Neurus1970 <cristian.f.medrano@gmail.com>
2022-11-11 15:44:30 -0300

    fixing links for multimedia support

--- scoring.js
+++ scoring.js
@@ -25,6 +25,28 @@ fs.createReadStream("resources/ResultadoScoringIndividuos.csv")
   .on("end", (err) => {
     if (err) console.log("An error has occurred");
     else {
+      result.forEach(v => {
+
+        v['id'] = v['id'].replace(/^0+/, '');
+        v['name'] = v['name'].replace(/\s\s+/g, ' ').replace(' ,', ',').trim();
+
+        v['defaultProbability12months'] = v['defaultProbability12months']/100.0;
+
+        var default_probabilities = {
+          months_3: undefined,
+          months_6: undefined,
+          months_9: undefined,
+          months_12: v['defaultProbability12months']
+        }
+        v.default_probabilities = default_probabilities;
+
+        var links = {
+          href:"/models/scoring/individuals/"+v.id
+        };
+        v['_links'] = links;
+
+        delete v['defaultProbability12months'];
+      });
       console.log("Recibido nuevo modelo de prediccion de score futuro!");
     }
   });
@@ -36,21 +58,22 @@ app.listen(port, () => console.log(`model scoring API listening on port ${port}!
 app.get('/models/scoring/individuals/:id', (req, res) => {
 
   var initialTime = new Date();
-  var deudores = result.findByValueOfObject("NRO_DOCUMENTO_1", req.params.id.padStart(18, "0"));
+  
+  var deudores = result.findByValueOfObject("id", req.params.id);
+
   if (deudores.length == 0) {
     res.writeHead(404, {"Content-Type": "text/plain"});
     res.write("404 Not found");
     res.end();
-  } else
-    deudores[0].NOMBRE_DEUDOR = deudores[0].NOMBRE_DEUDOR.replace(/\s\s+/g, ' ').replace(' ,', ',');
-    
-    var salida = {
+  } else {
+   var salida = {
       searchTime: new Date().getTime() - initialTime.getTime(),
       hits: deudores.length,
       debtors: deudores
     }
-
     res.status(200).json(salida);
+  }
+
 });
 
 
@@ -66,10 +89,10 @@ app.get('/models/scoring/individuals', (req, res) => {
     pageSize = parseInt(req.query.pageSize);
 
 
-  if (req.query.NOMBRE_DEUDOR !== undefined) {
-    names = req.query.NOMBRE_DEUDOR.toUpperCase().split(" ");
+  if (req.query.name !== undefined) {
+    names = req.query.name.toUpperCase().split(" ");
     result.forEach(v => {
-      if(names.every(name => v.NOMBRE_DEUDOR.includes(name))) deudores.push(v);
+      if(names.every(name => v.name.includes(name))) deudores.push(v);
     });
   } else
     deudores = result;
@@ -87,13 +110,11 @@ app.get('/models/scoring/individuals', (req, res) => {
     else
       pageNumber = parseInt(req.query.page)-1;
 
-
     var offset = pageNumber*pageSize;
     var lastRecord = (offset+pageSize > deudores.length ? deudores.length : offset+pageSize);
 
     for (i=offset; i<lastRecord; i++) {
       deudor = deudores[i];
-      deudor.NOMBRE_DEUDOR = deudor.NOMBRE_DEUDOR.replace(/\s\s+/g, ' ').replace(' ,', ',');
       deudoresAmostrar.push(deudor);
     }
 
@@ -105,16 +126,15 @@ app.get('/models/scoring/individuals', (req, res) => {
       nextPage = '/models/scoring/individuals?page='.concat(pageNumber+2);
       prevPage = '/models/scoring/individuals?page='.concat(pageNumber);
 
-      if (req.query.NOMBRE_DEUDOR !== undefined) {
-        nextPage = nextPage.concat("&NOMBRE_DEUDOR=").concat(req.query.NOMBRE_DEUDOR).replace(" ", "%20");
-        prevPage = prevPage.concat("&NOMBRE_DEUDOR=").concat(req.query.NOMBRE_DEUDOR).replace(" ", "%20");
+      if (req.query.name !== undefined) {
+        nextPage = nextPage.concat("&name=").concat(req.query.name).replace(" ", "%20");
+        prevPage = prevPage.concat("&name=").concat(req.query.name).replace(" ", "%20");
       }
 
       if (req.query.pageSize !== undefined) {
         nextPage = nextPage.concat("&pageSize=").concat(req.query.pageSize);
         prevPage = prevPage.concat("&pageSize=").concat(req.query.pageSize);
       }
-
 
       var salida = {
         searchTime: new Date().getTime() - initialTime.getTime(),
